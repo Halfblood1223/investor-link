51d82c0ea1b3aee249a3f27f506f7021bfcf02b0
Yash Semlani <yashveersemlan@gmail.com>
2021-05-05 13:59:04 -0500

    Editing WSGI

--- vancouvar/wsgi.py
+++ vancouvar/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vancouvar.settings')
 
-application = get_wsgi_application()
+from helloworld.wsgi import HelloWorldApplication
+
+application = HelloWorldApplication(application)
