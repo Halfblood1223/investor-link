870add6ef424add36c2f746489e1b48dabee6217
Yash Semlani <yashveersemlan@gmail.com>
2021-05-06 11:41:31 -0500

    Removing WSGI changes

--- vancouvar/wsgi.py
+++ vancouvar/wsgi.py
@@ -9,18 +9,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 
 import os, sys
 
-# add the hellodjango project path into the sys.path
-sys.path.append('<PATH_TO_MY_DJANGO_PROJECT>/vancouvar')
-
-# add the virtualenv site-packages path to the sys.path
-sys.path.append('<PATH_TO_VIRTUALENV>/Lib/site-packages')
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vancouvar.settings')
 
 application = get_wsgi_application()
-
-from helloworld.wsgi import HelloWorldApplication
-
-application = HelloWorldApplication(application)
