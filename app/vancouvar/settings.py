a39c9389b168d1baf1beba5a30d757a5bfa8dd77
Yash Semlani <yashveersemlan@gmail.com>
2021-05-08 19:14:09 -0500

    Possible Final Docker Fix, Local Machine Works, Static File Delivery Through NGINX Not Configured yet

--- app/vancouvar/settings.py
+++ app/vancouvar/settings.py
@@ -154,11 +154,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/static'
-MEDIA_URL = '/static/media'
-
-STATIC_ROOT = 'vol/web/static'
-MEDIA_ROOT = '/vol/web/media'
+STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
